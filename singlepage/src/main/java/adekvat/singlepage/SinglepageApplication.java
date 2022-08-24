b118a28df4392d75d7b3b8c043bae47b71f1e6cd
simonhaughom <simon.haughom@gmail.com>
2022-08-24 17:03:10 +0200

    ny main for front og back

---
+++ singlepage/src/main/java/adekvat/singlepage/SinglepageApplication.java
@@ -0,0 +1,27 @@
+package adekvat.singlepage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import adekvat.singlepage.model.User;
+import adekvat.singlepage.repository.UserRepository;
+
+@SpringBootApplication
+public class SinglepageApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinglepageApplication.class, args);
+	}
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.userRepository.save(new User("Tom", "Bombadil", "tom.bombadil@gmail.com"));
+		this.userRepository.save(new User("Thorin", "Oakenshield", "thorin.oakenshield@gmail.com"));
+		this.userRepository.save(new User("Peregrin", "Took", "foolofatook@gmail.com"));
+	}
+}
---
+++ singlepage/src/main/java/adekvat/singlepage/controller/UserController.java
@@ -0,0 +1,26 @@
+package adekvat.singlepage.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import adekvat.singlepage.model.User;
+import adekvat.singlepage.repository.UserRepository;
+
+@CrossOrigin(origins = "http://localhost:3000/")
+@RestController
+@RequestMapping("api/")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("users")
+    public List<User> getUser() {
+        return this.userRepository.findAll();
+    }
+}
